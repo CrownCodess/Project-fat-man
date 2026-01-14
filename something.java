141307bf090e179f030a6c8b37dbfdaab9ab7ebb
CrownCodess <roderickcchen@gmail.com>
2026-01-13 22:48:59 -0800

    Update and rename main.java to something.java

---
+++ something.java
@@ -0,0 +1,13 @@
+public class something{ public static void main(String[] args){ int something = 1;
+        for (;;){
+            try{
+                int pointless = (1 / something);
+                for (int i = 0; i < 100; i ++) System.out.print("🤡"); 
+                if (something == 1) something = 0;
+                else something = 1;}
+            catch(ArithmeticException e){
+                for(;;){
+                    for(int j = 0; j < 100; j ++)  System.out.print("😈");
+                    something = 1;
+                    break;
+                }}}}}
